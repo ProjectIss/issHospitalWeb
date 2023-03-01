a8d101bc2f2a711addd424d337b32f2b4741ffe4
PC <PC@LAPTOP-N3JRILH5>
2023-03-01 18:46:01 +0530

    intex pages alone we did

---
+++ issHospital/Controllers/ItemSetupController.cs
@@ -0,0 +1,91 @@
+﻿using issHospital_Utility.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace issHospital.Controllers
+{
+    public class ItemSetupController : Controller
+    {
+        // GET: ItemSetup
+        public ActionResult Index()
+        {
+           
+            return View();
+        }
+
+        // GET: ItemSetup/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ItemSetup/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ItemSetup/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ItemSetup/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ItemSetup/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ItemSetup/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ItemSetup/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ issHospital_Utility/DTOs/ItemSetupDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace issHospital_Utility.DTOs
+{
+   public class ItemSetupDTO
+    {
+        public int Id { get; set; }
+        public int Reorder { get; set; }
+        public int Tax { get; set; }
+        public int price { get; set; }
+        public string Menu { get; set; }
+        public string Commcode { get; set; }
+    }
+}
